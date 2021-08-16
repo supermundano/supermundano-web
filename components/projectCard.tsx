bc7260bd17871a1161fc7c4f867f8e3509dba1c2
Carlos Faria <carlos@supermundano.com>
2021-08-16 17:12:37 +0200

    Utilizamos nex-sanity-image para cargar imágenes de proyectos en componente projectCard

--- components/projectCard.tsx
+++ components/projectCard.tsx
@@ -2,15 +2,11 @@ import styled from 'styled-components'
 import Project from '../models/project'
 import Link from 'next/link'
 import Image from 'next/image'
-import {getImageDimensions} from '@sanity/asset-utils'
-import { urlFor } from '../lib/sanity'
-
+import { getImage } from '@sanity/asset-utils'
+import { sanityClient } from '../lib/sanity'
+import { useNextSanityImage } from 'next-sanity-image';
 
 const ProjectCardStyle = styled.article`
-  a{
-
-  }
-
   img{
     width: 100%;
     height: auto;
@@ -23,26 +19,31 @@ const ProjectCardStyle = styled.article`
 `;
 
 export default function ProjectCard(project:Project) {
+  const listImage = getImage(project.list_image)
+  const imageProps = useNextSanityImage(
+		sanityClient,
+		listImage,
+    {
+			blurUpImageWidth: 200,
+			blurUpImageQuality: 40,
+			blurUpAmount: 24
+		}
+	);
+  const src = (typeof imageProps?.src === 'string') ? imageProps?.src : '';
 
-    var slug = `project/${project.slug}`;
-    const ref = project.list_image
-    const imageDimensions = getImageDimensions(ref)
-    const placeholderUrl = urlFor(ref).width(200).url()
-    const strPlaceholderUrl = (typeof placeholderUrl === 'string') ? placeholderUrl : '';
-    const imageURL = urlFor(ref)?.url()
-    const strImageUrl = (typeof imageURL === 'string') ? imageURL : '';
+  var slug = `/project/${project.slug}`;
 
-    return (
-      <ProjectCardStyle>
-        <Link href={slug}>
-            <a>
-              <Image alt="Supermundano" src={ strImageUrl } layout="responsive" width={imageDimensions.width} height={imageDimensions.height} sizes="(max-width: 800px) 100vw, 800px" placeholder="blur" blurDataURL={strPlaceholderUrl} />
-            </a>
-        </Link>
-        <h2 className="title">
-          <Link href={slug}><a>{project.title}</a></Link>
-        </h2>
-        <p className="description">{project.description}</p>
-      </ProjectCardStyle>
-    )
+  return (
+    <ProjectCardStyle>
+      <Link href={slug}>
+          <a>
+            <Image alt="Supermundano" src={src} {...imageProps} layout="responsive" sizes="(max-width: 800px) 100vw, 800px" />
+          </a>
+      </Link>
+      <h2 className="title">
+        <Link href={slug}><a>{project.title}</a></Link>
+      </h2>
+      <p className="description">{project.description}</p>
+    </ProjectCardStyle>
+  )
 }
