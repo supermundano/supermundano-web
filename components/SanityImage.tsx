4ca13d2cbb951ce501238b3e386e5f2bdc70742f
Carlos Faria <carlos@supermundano.com>
2021-08-16 17:42:13 +0200

    Fix blur para sanity images obtenidas desde contentRaw

--- components/SanityImage.tsx
+++ components/SanityImage.tsx
@@ -1,18 +1,22 @@
 import Image from 'next/image'
-import {getImageDimensions} from '@sanity/asset-utils'
-import { urlFor } from '../lib/sanity'
+import { sanityClient } from '../lib/sanity'
+import { useNextSanityImage } from 'next-sanity-image';
 
 export default function SanityImage( image_data :any) {
-
-  const ref = image_data.image_data?.asset?._ref
-  const imageDimensions = getImageDimensions(ref)
-  const placeholderUrl = urlFor(ref).width(200).url()
-  const strPlaceholderUrl = (typeof placeholderUrl === 'string') ? placeholderUrl : '';
-  const imageURL = urlFor(ref).url()
-  const strImageUrl = (typeof imageURL === 'string') ? imageURL : '';
+  const image_ref = image_data.image_data.asset._ref;
+  const imageProps = useNextSanityImage(
+		sanityClient,
+		image_ref,
+    {
+			blurUpImageWidth: 200,
+			blurUpImageQuality: 40,
+			blurUpAmount: 24
+		}
+	);
+  const src = (typeof imageProps?.src === 'string') ? imageProps?.src : '';
   const altText = image_data.image_data?.asset?.altText || 'Imagen Supermundano';
 
   return (
-      <Image alt={altText} src={ strImageUrl } layout="responsive" width={imageDimensions.width} height={imageDimensions.height} sizes="(max-width: 800px) 100vw, 800px" placeholder="blur" blurDataURL={strPlaceholderUrl} />
+      <Image alt={altText} src={src} {...imageProps} layout="responsive" sizes="(max-width: 800px) 100vw, 800px" />
   );
 }
