e321842db0d5aa6efdf839be7ed0e94b4d812e2b
Carlos Faria <carlos@supermundano.com>
2021-08-16 13:21:26 +0200

    Add wrapper en imagen destacada del proyecto para que funcione el layout responsive del componente Image de Nextjs

--- components/projectFeaturedInfo.tsx
+++ components/projectFeaturedInfo.tsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import Image from 'next/image'
+import {getImageDimensions} from '@sanity/asset-utils'
 
 // TODO: font-weight: 200; no está definido
 // TODO: Por qué project-image es un display: flex con justify-content: end ???
@@ -20,39 +21,42 @@ const ProjectFeaturedInfoStyle = styled.div`
     margin: 0;
   }
 
-  .project-image{
+  /* .project-image{
     display: flex;
     justify-content: end;
-  }
+  } */
 `;
 
 // TODO: .widget-title debería tener un nombre mejor, no estamos dentro de un widget
 // TODO: El h3 debería ser un h1
 export default function ProjectFeaturedInfo( {title, services, colabs, featured_image}:any ) {
+    const imageDimensions = getImageDimensions(featured_image.asset);
+
     return(
         <ProjectFeaturedInfoStyle>
           <div className="project-info">
 
-              <h3 className="widget-title">{ title }</h3>
-              <div className="project-services">
-                  <h2>Services</h2>
-                  {Object.entries(services).map(function(servicio, num){
-                      return (
-                          <p key={num}>{ servicio[1] } </p>
-                      )
-                  })}
-              </div>
+            <h3 className="widget-title">{ title }</h3>
+            <div className="project-services">
+              <h2>Services</h2>
+              {Object.entries(services).map(function(servicio, num){
+                return (
+                    <p key={num}>{ servicio[1] } </p>
+                )
+              })}
+            </div>
 
-              <div className="project-colabs">
-                  <h2>Colabs</h2>
-                  <p>{ colabs }</p>
-              </div>
+            <div className="project-colabs">
+              <h2>Colabs</h2>
+              <p>{ colabs }</p>
+            </div>
 
           </div>
 
           <div className="project-image">
-
-              <Image alt={title} src={featured_image.asset?.url} width="1161" height="693" />
+            <div className="wrap">
+              <Image alt={title} src={ featured_image?.asset?.url } layout="responsive" width={imageDimensions.width} height={imageDimensions.height} sizes="(max-width: 800px) 100vw, 800px" />
+            </div>
           </div>
 
         </ProjectFeaturedInfoStyle>
