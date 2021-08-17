5162875e47e374eb1ef9c198b721602da6b55312
Carlos Faria <carlos@supermundano.com>
2021-08-17 09:28:35 +0200

    Fix intento de solucionar el FOUC generado por Nextjs + Styled Components utilizando _document

---
+++ pages/_document.tsx
@@ -0,0 +1,33 @@
+// Fix for styled-components FOUC. Render the styles server side first using ServerStyleSheet component and serve the document with the initial styles
+// https://nextjs.org/docs/advanced-features/custom-document
+// https://github.com/vercel/next.js/blob/master/examples/with-styled-components/pages/_document.js
+import Document, { DocumentContext } from 'next/document'
+import { ServerStyleSheet } from 'styled-components'
+
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const sheet = new ServerStyleSheet()
+    const originalRenderPage = ctx.renderPage
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
+        })
+
+      const initialProps = await Document.getInitialProps(ctx)
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      }
+    } finally {
+      sheet.seal()
+    }
+  }
+}
