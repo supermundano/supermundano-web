e9fe3b560356992d417cf46849e92145de4b16e6
Carlos Faria <carlos@supermundano.com>
2021-08-19 08:30:34 +0200

    Clean desestructuración componente MenuLink

--- components/menuItem.tsx
+++ components/menuItem.tsx
@@ -1,12 +1,9 @@
 import MenuLink  from '../models/links'
-import styled, { withTheme } from 'styled-components'
+import styled from 'styled-components'
 import Link from 'next/link'
-import { motion } from 'framer-motion';
 
-// TODO: Colores con CSS Vars
 // TODO: Refactorizar (o directamente borrar la clase) MenuLink. Utilizar interface para tipado y pasar los datos en un objeto para desestructurarlos en la definición de MenuItem  https://nextjs.org/docs/api-reference/next/link#if-the-child-is-a-custom-component-that-wraps-an-a-tag
 const MenuItemStyles = styled.li`
-
   a{
     cursor: pointer;
     font-weight: 400;
@@ -14,12 +11,13 @@ const MenuItemStyles = styled.li`
 `;
 
 export default function MenuItem(menuLink:MenuLink) {
+  const {url, text} = menuLink
 
   return  (
     <MenuItemStyles>
       <span>
-        <Link href={menuLink.url} passHref>
-          <a>{menuLink.text}</a>
+        <Link href={url} >
+          <a>{text}</a>
         </Link>
       </span>
     </MenuItemStyles>
