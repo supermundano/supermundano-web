dec3ec8a4fcd835f719d363cc411ee68e28993de
Carlos Faria <carlos@supermundano.com>
2021-08-19 09:07:52 +0200

    Cambio carpeta para forzar archivos a mayusculas

--- pages/services/index.tsx
+++ pages/services/index.tsx
@@ -1,9 +1,6 @@
 import Page from '../../components/Page'
 import ServicesTextSection from '../../components/ServicesTextSection'
 import ServicesListSection from '../../components/ServicesListSection'
-import { getSanityContent } from '../../lib/sanity';
-import { featuredProjectsQuery } from '../../lib/queries';
-
 
 export default function Servicios( ) {
   return (
