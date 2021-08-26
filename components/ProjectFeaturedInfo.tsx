e75be3046e6aefcc8909bfe084b49aab03be28bb
Carlos Faria <carlos@supermundano.com>
2021-08-26 09:27:48 +0200

    Fix saltos de línea en colaboradores proyecto

--- components/ProjectFeaturedInfo.tsx
+++ components/ProjectFeaturedInfo.tsx
@@ -3,6 +3,7 @@ import Image from 'next/image'
 import { getImage } from '@sanity/asset-utils'
 import { sanityClient } from '../lib/sanity'
 import { useNextSanityImage } from 'next-sanity-image';
+import { Fragment } from 'react'
 
 const ProjectFeaturedInfoStyle = styled.div`
 
@@ -101,25 +102,24 @@ export default function ProjectFeaturedInfo( {title, services, colabs, featured_
     return(
         <ProjectFeaturedInfoStyle>
           <div className="project-info">
-
-              <h1 className="secondary-title">{ title }</h1>
-              <div className="project-info-columns">
-                <div className="project-services">
-                    <h2>Services</h2>
-                    {Object.entries(services).map(function(servicio, num){
-                        return (
-                            <p key={num}>{ servicio[1] } </p>
-                        )
-                    })}
-                </div>
-
-                <div className="project-colabs">
-                    <h2>Colabs</h2>
-                    <p>{ colabs }</p>
-                </div>
-
+            <h1 className="secondary-title">{ title }</h1>
+            <div className="project-info-columns">
+              <div className="project-services">
+                  <h2>Services</h2>
+                  {Object.entries(services).map(function(servicio, num){
+                      return (
+                          <p key={num}>{ servicio[1] } </p>
+                      )
+                  })}
               </div>
 
+              <div className="project-colabs">
+                  <h2>Colabs</h2>
+                  {colabs.split('\n').map((item:string, key:string) => {
+                    return <p key={key} >{item}</p>
+                  })}
+              </div>
+            </div>
           </div>
 
           <div className="project-image">
