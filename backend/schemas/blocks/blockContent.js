1c6967c0592c545ada0dcc9dc4956d2f69a22451
Carlos Faria <carlos@supermundano.com>
2021-08-27 08:55:53 +0200

    Anchors con capacidad para target _blank en contenido proyecto

--- backend/schemas/blocks/blockContent.js
+++ backend/schemas/blocks/blockContent.js
@@ -3,7 +3,47 @@ const BlockContent = {
   name: 'blockContent',
   type: 'array',
   of: [
-    { type: 'block' },
+    {
+      type: 'block',
+      marks: {
+        annotations: [
+          {
+            name: 'link',
+            type: 'object',
+            title: 'External link',
+            fields: [
+              {
+                name: 'href',
+                type: 'url',
+                title: 'URL'
+              },
+              {
+                title: 'Open in new tab',
+                name: 'blank',
+                description: 'Read https://css-tricks.com/use-target_blank/',
+                type: 'boolean'
+              }
+            ]
+          },
+          {
+            name: 'internalLink',
+            type: 'object',
+            title: 'Internal link',
+            fields: [
+              {
+                name: 'reference',
+                type: 'reference',
+                title: 'Reference',
+                to: [
+                  { type: 'post' },
+                  // other types you may want to link to
+                ]
+              }
+            ]
+          }
+        ]
+      }
+    },
     { type: 'twoImages' },
     { type: 'image' },
   ]
