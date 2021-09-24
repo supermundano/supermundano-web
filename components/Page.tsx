75524feb9f6b1349d59044bb3764199b5a9cf6ff
Joshua Torres <jtorres@supermundano.com>
2021-09-24 10:30:15 +0200

    Añadidas cookies

--- components/Page.tsx
+++ components/Page.tsx
@@ -3,8 +3,8 @@ import Header from './Header'
 import Footer from './Footer'
 import { motion } from 'framer-motion'
 
-// import CookieConsent, { Cookies } from "react-cookie-consent";
-// import Link from 'next/link'
+import CookieConsent, { Cookies } from "react-cookie-consent";
+import Link from 'next/link'
 
 export default function Page({ children }:any) {
 
@@ -41,7 +41,7 @@ export default function Page({ children }:any) {
       <Footer/>
 
 
-    {/* <CookieConsent
+    <CookieConsent
       location="bottom"
       buttonText="I agree"
       buttonClasses="cookie-btn"
@@ -53,7 +53,7 @@ export default function Page({ children }:any) {
       <p>We use cookies on our website to give you the most relevant experience by remembering your preferences and visits. By clicking &quot;I agree&quot;, you agree to the use of all cookies. </p>
 
       <p>You can get more info in our <Link href="/cookies"><a>cookies policy</a></Link></p>
-    </CookieConsent> */}
+    </CookieConsent>
 
     </motion.div>
     </>
