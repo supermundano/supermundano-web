e16f76371c4d6d3c9643ad147091778d9d0e069d
Joshua Torres <jtorres@supermundano.com>
2021-09-24 11:04:34 +0200

    Importación dinámica componente cookies

--- components/Page.tsx
+++ components/Page.tsx
@@ -1,10 +1,14 @@
-import Head from 'next/head'
-import Header from './Header'
-import Footer from './Footer'
-import { motion } from 'framer-motion'
+import Head from 'next/head';
+import Header from './Header';
+import Footer from './Footer';
+import { motion } from 'framer-motion';
+import dynamic from 'next/dynamic';
+import Link from 'next/link';
+import { lazy, Suspense } from 'react';
 
-import CookieConsent, { Cookies } from "react-cookie-consent";
-import Link from 'next/link'
+// import CookieConsent from "react-cookie-consent";
+// const CookieConsent = lazy(() => import('../node_modules/react-cookie-consent/build/index'));
+const CookieConsent = dynamic(() => import('../node_modules/react-cookie-consent/build/index'));
 
 export default function Page({ children }:any) {
 
@@ -40,21 +44,19 @@ export default function Page({ children }:any) {
         <main>{children}</main>
       <Footer/>
 
+      <CookieConsent
+        location="bottom"
+        buttonText="I agree"
+        buttonClasses="cookie-btn"
+        cookieName="accept-blavet-cookies"
+        sameSite="strict"
+        style={{ background: "#ffffffee", color: "#000", maxWidth: "380px", right: "0", left: "auto", padding: '2rem' }}
+        expires={150}
+      >
+        <p>We use cookies on our website to give you the most relevant experience by remembering your preferences and visits. By clicking &quot;I agree&quot;, you agree to the use of all cookies. </p>
 
-    <CookieConsent
-      location="bottom"
-      buttonText="I agree"
-      buttonClasses="cookie-btn"
-      cookieName="accept-blavet-cookies"
-      sameSite="strict"
-      style={{ background: "#ffffffee", color: "#000", maxWidth: "380px", right: "0", left: "auto", padding: '2rem' }}
-      expires={150}
-    >
-      <p>We use cookies on our website to give you the most relevant experience by remembering your preferences and visits. By clicking &quot;I agree&quot;, you agree to the use of all cookies. </p>
-
-      <p>You can get more info in our <Link href="/cookies"><a>cookies policy</a></Link></p>
-    </CookieConsent>
-
+        <p>You can get more info in our <Link href="/cookies"><a>cookies policy</a></Link></p>
+      </CookieConsent>
     </motion.div>
     </>
   )
