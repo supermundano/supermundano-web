e87f83668b90b31389b1cc124ac27751c3af66a4
Joshua Torres <jtorres@supermundano.com>
2021-09-30 17:38:50 +0200

    OG Tags adjust

--- pages/services/index.tsx
+++ pages/services/index.tsx
@@ -3,7 +3,7 @@ import ServicesTextSection from '../../components/ServicesTextSection'
 import ServicesListSection from '../../components/ServicesListSection'
 
 export default function Servicios( ) {
-  const ogTags:any = {"twitterHandle" : '', "currentURL" : '', "previewImage" : '', "siteName" : "Blavet Studio", "pageTitle" : "Blavet", "description": "Creative Brands in Context"};
+  const ogTags:any = {"twitterHandle" : '', "previewImage" : '', "siteName" : "Blavet Studio", "pageTitle" : "Blavet", "description": "Creative Brands in Context"};
   return (
     <div className="services">
       <Page ogTags={ogTags}>
