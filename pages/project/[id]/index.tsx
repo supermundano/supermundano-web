ad32f35b4fadc7e8b5540b9fcb3b2a18e4428e39
Carlos Faria <carlos@supermundano.com>
2021-10-04 09:30:59 +0200

    Fix perdida estilos página por carga dinámica

--- pages/project/[id]/index.tsx
+++ pages/project/[id]/index.tsx
@@ -3,10 +3,10 @@ import { getSanityContent } from '../../../lib/sanity'
 import { allProjectsQuery, singleProjectQuery } from '../../../lib/queries'
 import { GetStaticPaths, GetStaticProps } from 'next'
 import { createGlobalStyle } from 'styled-components'
-// import Page from '../../../components/Page'
+import Page from '../../../components/Page'
 // import ProjectFeaturedInfo from '../../../components/ProjectFeaturedInfo'
 // import ProjectEditorInfo from '../../../components/ProjectEditorInfo'
-const Page = dynamic(() => import('../../../components/Page'))
+// const Page = dynamic(() => import('../../../components/Page'))
 const ProjectFeaturedInfo = dynamic(() => import('../../../components/ProjectFeaturedInfo'))
 const ProjectEditorInfo = dynamic(() => import('../../../components/ProjectEditorInfo'))
 
